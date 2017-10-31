20951c1d92d4f585894c7bf12ddbeefcc34a272d
Seth Bresnett <sgb@softwire.com>
2017-10-31 10:32:58 +0000

    Created a bit of mobility for the knight and a test to go along with it.

--- src/main/java/training/chessington/model/pieces/Knight.java
+++ src/main/java/training/chessington/model/pieces/Knight.java
@@ -1,5 +1,6 @@
 package training.chessington.model.pieces;
 
+import javafx.scene.layout.CornerRadii;
 import training.chessington.model.Board;
 import training.chessington.model.Coordinates;
 import training.chessington.model.Move;
@@ -15,6 +16,36 @@ public class Knight extends AbstractPiece {
 
     @Override
     public List<Move> getAllowedMoves(Coordinates from, Board board) {
-        return new ArrayList<>();
+        List<Move> moveList = new ArrayList<>();
+
+        if (getColour().equals(PlayerColour.WHITE) && from.getRow() == 0) {
+            return moveList;
+        }
+        if (getColour().equals(PlayerColour.BLACK) && from.getCol() == 7) {
+            return moveList;
+        }
+        // as they always start in the same place make it so that the they cannot run off the map
+        // similar rules to pawn inverse where the first move it moves less and after it can move more.
+        if (colour == PlayerColour.WHITE) {
+            Coordinates horseMove1 = new Coordinates(from.getRow() - 1, from.getCol() + 2);
+            Coordinates horseMove2 = new Coordinates(from.getRow() - 2, from.getCol() + 1);
+            Move move = new Move(from, horseMove1);
+            Move move2 = new Move(from, horseMove2);
+            moveList.add(move);
+            moveList.add(move2);
+        }
+        if(colour == PlayerColour.BLACK) {
+            Coordinates blackHorse1 = new Coordinates(from.getRow() + 1, from.getCol() + 2);
+            Coordinates blackHorse2 = new Coordinates(from.getRow() + 2, from.getCol() + 1);
+            Move blackMove = new Move(from, blackHorse1);
+            Move blackMove2 = new Move(from, blackHorse2);
+            moveList.add(blackMove);
+            moveList.add(blackMove2);
+
+        }
+
+        return moveList;
+
+
     }
 }
--- src/test/java/training/chessington/model/pieces/KnightTest.java
+++ src/test/java/training/chessington/model/pieces/KnightTest.java
@@ -3,5 +3,29 @@ package training.chessington.model.pieces;
 import static training.chessington.model.pieces.PieceAssert.*;
 import static org.assertj.core.api.Assertions.*;
 
+import org.junit.Test;
+import training.chessington.model.Board;
+import training.chessington.model.Coordinates;
+import training.chessington.model.PlayerColour;
+import training.chessington.model.Move;
+import training.chessington.*;
+import java.util.List;
+
 public class KnightTest {
+
+
+    @Test
+    public void knightCanMoveGenerally() {
+        //Arrange
+        Board board = Board.empty();
+        Piece knight = new Knight(PlayerColour.WHITE);
+        Coordinates coords = new Coordinates(5,2 );
+        board.placePiece(coords, knight);
+
+        //Act
+        List<Move> moves = knight.getAllowedMoves(coords, board);
+
+        //Assert
+        assertThat(moves).contains(new Move(coords, coords.plus(-1, 2)));
+    }
 }
